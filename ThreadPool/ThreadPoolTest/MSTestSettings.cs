317ad7617fc37ac990f46aeda2bfb8211185889b
indeec <murathamatanov@gmail.com>
2026-01-12 21:10:39 +0300

    fix file

--- ThreadPool/ThreadPoolTest/MSTestSettings.cs
+++ ThreadPool/ThreadPoolTest/MSTestSettings.cs
@@ -1,4 +1,3 @@
 ﻿// Copyright (c) Murat Khamatyanov. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 [assembly: Parallelize(Workers = 1, Scope = ExecutionScope.ClassLevel)]
